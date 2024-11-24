e49b22b4534b94c8da7ee6ed00cec7963bde4556
Markus Schepke <markus.schepke@wolt.com>
2024-11-24 23:18:45 +0200

    Added to_str() and normalize_space() to utils

--- src/board_game_scraper/utils/__init__.py
+++ src/board_game_scraper/utils/__init__.py
@@ -1,9 +1,12 @@
 from board_game_scraper.utils.dates import now
 from board_game_scraper.utils.parsers import parse_date, parse_float, parse_int
+from board_game_scraper.utils.strings import normalize_space, to_str
 
 __all__ = [
+    "normalize_space",
     "now",
     "parse_date",
     "parse_float",
     "parse_int",
+    "to_str",
 ]
