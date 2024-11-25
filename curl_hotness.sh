9ac1cef54d5c0bbdc8ea1eeac4b44183df3f6f82
Markus Schepke <markus.schepke@wolt.com>
2024-11-25 22:56:38 +0200

    Added cron/games.recommend.curl_hotness.plist

--- curl_hotness.sh
+++ curl_hotness.sh
@@ -7,9 +7,9 @@ URL='https://boardgamegeek.com/xmlapi2/hot?type=boardgame'
 BASE_DIR="${HOME}/Recommend.Games/board-game-scraper"
 DEFAULT_DIR="${BASE_DIR}/feeds/bgg_hotness/raw"
 DEST_DIR="${1:-${DEFAULT_DIR}}"
-DEST_FILE="${DEST_DIR}/$(date --utc '+%Y-%m-%dT%H-%M-%S').xml"
+DEST_FILE="${DEST_DIR}/$(date -u '+%Y-%m-%dT%H-%M-%S').xml"
 
 echo "Saving latest BGG hotness from <${URL}> to ${DEST_FILE}…"
-mkdir --parents "${DEST_DIR}"
+mkdir -p "${DEST_DIR}"
 curl "${URL}" --compressed --output "${DEST_FILE}"
 echo "Done."
