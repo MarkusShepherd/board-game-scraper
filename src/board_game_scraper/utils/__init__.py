c9f068659a77f5ef340c935962ff2d22be650886
Markus Schepke <markus.schepke@wolt.com>
2024-11-30 23:11:38 +0200

    Added utils.files.extract_field_from_jsonlines_file()

--- src/board_game_scraper/utils/__init__.py
+++ src/board_game_scraper/utils/__init__.py
@@ -1,8 +1,10 @@
 from board_game_scraper.utils.dates import now
+from board_game_scraper.utils.files import extract_field_from_jsonlines_file
 from board_game_scraper.utils.parsers import parse_date, parse_float, parse_int
 from board_game_scraper.utils.strings import lower_or_none, normalize_space, to_str
 
 __all__ = [
+    "extract_field_from_jsonlines_file",
     "lower_or_none",
     "normalize_space",
     "now",
---
+++ src/board_game_scraper/utils/files.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import json
+import logging
+from typing import TYPE_CHECKING, Any, Callable
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+    from pathlib import Path
+
+LOGGER = logging.getLogger(__name__)
+
+
+def extract_field_from_jsonlines_file(
+    *,
+    file_path: Path,
+    field: str,
+    encoding: str = "utf-8",
+    converter: Callable[[Any], Any] | None = None,
+) -> Generator[Any, None, None]:
+    with file_path.open(mode="r", encoding=encoding) as file:
+        for line in file:
+            try:
+                data = json.loads(line)
+                value = data.get(field)
+                if converter:
+                    value = converter(value)
+            except Exception:
+                LOGGER.exception("Error while parsing JSON line <%s>", line)
+                continue
+            if value is not None:
+                yield value
