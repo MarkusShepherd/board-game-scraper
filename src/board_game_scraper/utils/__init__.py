22df27643c9948945adca1a225524093a3581514
Markus Schepke <markus.schepke@wolt.com>
2024-11-30 23:29:07 +0200

    Added load_premium_users()

--- src/board_game_scraper/utils/__init__.py
+++ src/board_game_scraper/utils/__init__.py
@@ -2,6 +2,7 @@ from board_game_scraper.utils.dates import now
 from board_game_scraper.utils.files import (
     extract_field_from_csv_file,
     extract_field_from_jsonlines_file,
+    load_premium_users,
 )
 from board_game_scraper.utils.parsers import parse_date, parse_float, parse_int
 from board_game_scraper.utils.strings import lower_or_none, normalize_space, to_str
@@ -9,6 +10,7 @@ from board_game_scraper.utils.strings import lower_or_none, normalize_space, to_
 __all__ = [
     "extract_field_from_csv_file",
     "extract_field_from_jsonlines_file",
+    "load_premium_users",
     "lower_or_none",
     "normalize_space",
     "now",
--- src/board_game_scraper/utils/files.py
+++ src/board_game_scraper/utils/files.py
@@ -3,11 +3,18 @@ from __future__ import annotations
 import csv
 import json
 import logging
+from pathlib import Path
 from typing import TYPE_CHECKING, Any, Callable
 
+import yaml
+from scrapy.utils.misc import arg_to_iter
+
+from board_game_scraper.utils.dates import now
+from board_game_scraper.utils.parsers import parse_date
+
 if TYPE_CHECKING:
-    from collections.abc import Generator
-    from pathlib import Path
+    from collections.abc import Generator, Iterable
+    from datetime import datetime
 
 LOGGER = logging.getLogger(__name__)
 
@@ -48,3 +55,61 @@ def extract_field_from_csv_file(
                 value = converter(value)
             if value is not None:
                 yield value
+
+
+def _load_yaml(
+    path: str | Path,
+    encoding: str = "utf-8",
+) -> Iterable[dict[str, Any]]:
+    path = Path(path).resolve()
+    LOGGER.info("Loading YAML from <%s>", path)
+    try:
+        with path.open(encoding=encoding) as yaml_file:
+            yield from yaml.safe_load(yaml_file)
+    except Exception:
+        LOGGER.exception("Unable to load YAML from <%s>", path)
+
+
+def _load_yamls(
+    paths: Iterable[str | Path],
+    encoding: str = "utf-8",
+) -> Iterable[dict[str, Any]]:
+    for path in paths:
+        yield from _load_yaml(path, encoding)
+
+
+def load_premium_users(
+    dirs: str | Path | Iterable[str | Path] | None = None,
+    files: str | Path | Iterable[str | Path] | None = None,
+    compare_date: datetime | str | None = None,
+    encoding: str = "utf-8",
+) -> Iterable[str]:
+    """Load premium users from YAML files and compare against given date."""
+
+    compare_date = parse_date(compare_date) or now()
+    LOGGER.info("Comparing premium expiration dates against <%s>", compare_date)
+
+    for file_dir_str in arg_to_iter(dirs):
+        file_dir = Path(file_dir_str).resolve()
+        if file_dir.is_dir():
+            LOGGER.info("Loading YAML files from config dir <%s>", file_dir)
+            yield from load_premium_users(
+                files=file_dir.glob("*.yaml"),
+                compare_date=compare_date,
+                encoding=encoding,
+            )
+        else:
+            LOGGER.warning("Skipping non-existing config dir <%s>", file_dir)
+
+    for row in _load_yamls(arg_to_iter(files), encoding):
+        for username_upper, expiry_date_str in row.items():
+            username = username_upper.lower()
+            expiry_date = parse_date(expiry_date_str)
+            if not expiry_date or expiry_date < compare_date:
+                LOGGER.info(
+                    "Premium for user <%s> ended on <%s>",
+                    username,
+                    expiry_date,
+                )
+            else:
+                yield username
