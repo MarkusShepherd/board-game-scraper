3f6819994b79d5722a987a2f43c58826ddbc61f8
Markus Schepke <markus.schepke@wolt.com>
2024-11-30 23:19:34 +0200

    Added extract_field_from_csv_file()

--- src/board_game_scraper/utils/__init__.py
+++ src/board_game_scraper/utils/__init__.py
@@ -1,9 +1,13 @@
 from board_game_scraper.utils.dates import now
-from board_game_scraper.utils.files import extract_field_from_jsonlines_file
+from board_game_scraper.utils.files import (
+    extract_field_from_csv_file,
+    extract_field_from_jsonlines_file,
+)
 from board_game_scraper.utils.parsers import parse_date, parse_float, parse_int
 from board_game_scraper.utils.strings import lower_or_none, normalize_space, to_str
 
 __all__ = [
+    "extract_field_from_csv_file",
     "extract_field_from_jsonlines_file",
     "lower_or_none",
     "normalize_space",
--- src/board_game_scraper/utils/files.py
+++ src/board_game_scraper/utils/files.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import csv
 import json
 import logging
 from typing import TYPE_CHECKING, Any, Callable
@@ -28,5 +29,22 @@ def extract_field_from_jsonlines_file(
             except Exception:
                 LOGGER.exception("Error while parsing JSON line <%s>", line)
                 continue
+            if value is not None:
+                yield value
+
+
+def extract_field_from_csv_file(
+    *,
+    file_path: Path,
+    field: str,
+    encoding: str = "utf-8",
+    converter: Callable[[Any], Any] | None = None,
+) -> Generator[Any, None, None]:
+    with file_path.open(mode="r", encoding=encoding) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            value = row.get(field)
+            if converter:
+                value = converter(value)
             if value is not None:
                 yield value
